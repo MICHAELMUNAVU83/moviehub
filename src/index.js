3711be986067cba61d55965a7c5aa558e2dc586f
trance <michaelmunavu83@gmail.com>
2022-06-27 15:44:57 +0300

    add header h3

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './styles/main.css';
+import displayMovies from './modules/display.js';
+
+displayMovies();
---
+++ src/modules/display.js
@@ -0,0 +1,12 @@
+
+const displayMovies = async () => {
+    const request = await fetch('https://api.tvmaze.com/shows');
+    const requestmovies = await request.json();
+    requestmovies.forEach(
+        (movieData) =>{
+            
+            console.log(movieData);
+        }
+    )
+ }
+ export default displayMovies ;
