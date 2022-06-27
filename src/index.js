e91292299ac665ce19cde2dff2e006204e9d10f1
trance <michaelmunavu83@gmail.com>
2022-06-27 18:29:01 +0300

    Added number of likes to be displayed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './styles/main.css';
 import displayData from './modules/display.js';
 import modal from './modules/modal.js';
+import likesLoad from './modules/likesLoad.js';
 
 displayData();
+likesLoad();
 modal();
---
+++ src/modules/likesLoad.js
@@ -0,0 +1,23 @@
+const likesLoad = async function () {
+  const requestmovies = await fetch('https://api.tvmaze.com/shows');
+  const requestedMovies = await requestmovies.json();
+  requestedMovies.forEach(async (movieData) => {
+    const gotlikesapi = await fetch(
+      `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Hy9Xvp9M5vmOt13P3eGQ/likes/?item_id=${movieData.id}`,
+    );
+    const gotlikes = await gotlikesapi.json();
+
+    gotlikes.forEach((likess) => {
+      if (Number(likess.item_id) === movieData.id) {
+        document.querySelector(
+          `[id=like-content-${movieData.id}]`,
+        ).innerHTML = `
+  
+               <span>${likess.likes}</span> 
+               `;
+      }
+    });
+  });
+};
+
+export default likesLoad;
