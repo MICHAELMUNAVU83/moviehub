c1beff95c05d800cf0da91c02cb8eeaf0d2d7a21
Abel-desu <95746961+Abel-desu@users.noreply.github.com>
2022-06-28 12:58:18 +0300

    test comment

---
+++ tests/commentCounter.test.js
@@ -0,0 +1,19 @@
+import {commentCounter} from "../src/modules/__mocks__/commentCounter.js";
+
+describe("Comment counter", () => {
+  test ("each comment", async () => {
+    // arrange
+
+    const data1 = [
+      { id: 1, comment: "hello", username: "john" },
+      { id: 2, comment: "hello", username: "john" },
+      { id: 3, comment: "hello", username: "john" },
+      { id: 4, comment: "hello", username: "john" },
+    ];
+
+    // act
+    const data2 = await commentCounter(data1);
+    // assert
+    expect(data2).toBe(4);
+  });
+});
